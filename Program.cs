0e5423db1197e4404f07362479049831968b0ae9
Aiden <39316799+Tropxcal@users.noreply.github.com>
2023-03-27 16:49:01 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,65 +1,67 @@
-﻿using System;
+using System;
 
 
-namespace FirstProgram
-{
-    class program
-    {
-        static void Main(string[] args)
+
+        namespace FirstProgram
         {
-            Console.WriteLine("what method of opperation are you using");
-            Console.WriteLine("enter 1 for multiplication");
-            Console.WriteLine("enter 2 for divison");
-            Console.WriteLine("enter 3 for subtraction");
-            Console.WriteLine("enter 4 for addition");
-            Console.WriteLine("enter 5 for remainder");
-
-            int select = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Select first number");
-            int num_1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Select second number");
-            int num_2 = Convert.ToDouble(Console.ReadLine());
-
-            double total = 0;
-            switch (select)
+            class program
             {
-                case 1:
-                    int mult = num_1 * num_2;
-                    Console.WriteLine(mult);
-                    break;
-                    
+                static void Main(string[] args)
+                {
+                    Console.WriteLine("what method of opperation are you using");
+                    Console.WriteLine("enter 1 for multiplication");
+                    Console.WriteLine("enter 2 for divison");
+                    Console.WriteLine("enter 3 for subtraction");
+                    Console.WriteLine("enter 4 for addition");
+                    Console.WriteLine("enter 5 for remainder");
 
-                case 2:
-                   double div  = num_1 / num_2;
-                    Console.WriteLine(div);
-                    break;
+                    int select = Convert.ToInt32(Console.ReadLine());
 
-                case 3:
-                  double  sub = num_1 - num_2;
-                    Console.WriteLine(sub);
-                    break;
+                    Console.WriteLine("Select first number");
+            double num_1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Select second number");
+            double num_2 = Convert.ToDouble(Console.ReadLine());
 
-
-                case 4:
-                   double add = num_1 + num_2;
-                    Console.WriteLine(add);
-                    break;
-                case 5:
-                    double rem = num_1 % num_2;
-                    Console.WriteLine(rem);
-                    break;
-
-                default:
-                    Console.WriteLine(select + " is not an opption choose a diffrent method");
-                    break;
-
-                    
+                    double total = 0;
+                    switch (select)
+                    {
+                        case 1:
+                    double mult = num_1 * num_2;
+                            Console.WriteLine(mult);
+                            break;
 
 
+                        case 2:
+                            double div = num_1 / num_2;
+                            Console.WriteLine(div);
+                            break;
 
+                        case 3:
+                            double sub = num_1 - num_2;
+                            Console.WriteLine(sub);
+                            break;
+
+
+                        case 4:
+                            double add = num_1 + num_2;
+                            Console.WriteLine(add);
+                            break;
+                        case 5:
+                            double rem = num_1 % num_2;
+                            Console.WriteLine(rem);
+                            break;
+
+                        default:
+                            Console.WriteLine(select + " is not an opption choose a diffrent method");
+                            break;
+
+
+
+
+
+                    }
+                    Console.WriteLine(" Here is your answer have a nice day");
+                }
             }
-            Console.WriteLine(" Here is your answer have a nice day");
         }
-    }
-}
+ 
